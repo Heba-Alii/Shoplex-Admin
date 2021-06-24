74cee2d11a506533cdc5f2be15189595c9a8be28
Heba-ali95 <hebaaliali18@gmail.com>
2021-06-24 09:31:47 +0200

    Colors

--- Shoplex Admin App/assets/js/line-chart.js
+++ Shoplex Admin App/assets/js/line-chart.js
@@ -180,14 +180,14 @@ function showUsers(newUsers, deletedUser, labels){
                 labels: labels,
                 datasets: [{
                     label: "New User",
-                    backgroundColor: "rgba(117, 19, 246, 0.1)",
-                    borderColor: '#0b76b6',
+                    backgroundColor: "#1cb0ff",
+                   
                     data: newUsers,
                 },
                 {
                     label: "Deleted User",
-                    backgroundColor: "rgba(200, 0, 30, 0.75)",
-                    borderColor: '#0b76b6',
+                    backgroundColor: "#ff9633",
+                   
                     data: deletedUser,
                 },
               
@@ -234,61 +234,7 @@ function showUsers(newUsers, deletedUser, labels){
     }
 }
 
-/*--------------  coin_sales2 start ------------*/
-// if ($('#coin_sales2').length) {
-//     var ctx = document.getElementById("coin_sales2").getContext('2d');
-//     var chart = new Chart(ctx, {
-//         // The type of chart we want to create
-//         type: 'line',
-//         // The data for our dataset
-//         data: {
-//             labels: ["January", "February", "March", "April", "May", "June", "July", "January", "February", "March", "April", "May"],
-//             datasets: [{
-//                 label: "NewUser",
-//                 backgroundColor: "rgba(240, 180, 26, 0.1)",
-//                 borderColor: '#F0B41A',
-//                 data: [18, 41, 86, 49, 20, 65, 64, 50, 49, 30, 45, 26],
-//             }]
-//         },
-//         // Configuration options go here
-//         options: {
-//             legend: {
-//                 display: false
-//             },
-//             animation: {
-//                 easing: "easeInOutBack"
-//             },
-//             scales: {
-//                 yAxes: [{
-//                     display: !1,
-//                     ticks: {
-//                         fontColor: "rgba(0,0,0,0.5)",
-//                         fontStyle: "bold",
-//                         beginAtZero: !0,
-//                         maxTicksLimit: 5,
-//                         padding: 0
-//                     },
-//                     gridLines: {
-//                         drawTicks: !1,
-//                         display: !1
-//                     }
-//                 }],
-//                 xAxes: [{
-//                     display: !1,
-//                     gridLines: {
-//                         zeroLineColor: "transparent"
-//                     },
-//                     ticks: {
-//                         padding: 0,
-//                         fontColor: "rgba(0,0,0,0.5)",
-//                         fontStyle: "bold"
-//                     }
-//                 }]
-//             }
-//         }
-//     });
-// }
-/*--------------  coin_sales2 End ------------*/
+
 
 function showSellers(newSellers, deletedSellers, labels){
     if ($('#coin_sales2').length) {
@@ -301,14 +247,13 @@ function showSellers(newSellers, deletedSellers, labels){
                 labels: labels,
                 datasets: [{
                     label: "New Sellers",
-                    backgroundColor: "rgba(117, 19, 246, 0.1)",
-                    borderColor: '#0b76b6',
+                    backgroundColor: "#1cb0ff",
+             
                     data: newSellers,
                 },
                 {
                     label: "Deleted Sellers",
-                    backgroundColor: "rgba(200, 0, 30, 0.75)",
-                    borderColor: '#0b76b6',
+                    backgroundColor: "#ff9633",
                     data: deletedSellers,
                 }]
             },
